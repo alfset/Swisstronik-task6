4e74e677b0eaf50aad22334bde3fb924e35097a3
Alfino Setiawan <alfset5@gmail.com>
2024-08-24 14:07:10 +0000

    remove utils

--- utils/deployed-address.ts
+++
@@ -1,3 +0,0 @@
-export const TransparentUpgradeableProxy = '0x5A4a68A470780E578049C20d1B0FB00F72d29A9B'
-export const MyContractV1 = 'undefined'
-export const MyContractV2 = '0x22D1f28e75ba6317A954502D158BE93D4C706F87'
